5084b40fef08bdfedd4acba3e82f7102e435ea0a
mavharsha <sreeharsha.mamilla@gmail.com>
2015-04-20 23:20:56 -0700

    Day two, added three more

---
+++ ex7.py
@@ -0,0 +1,15 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # what'd that do?
+
+end1 = "H"
+end2 = "A"
+end3 = "R"
+end4 = "S"
+end5 = "H"
+end6 = "A"
+
+#watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3,
+print end4 + end5 + end6
